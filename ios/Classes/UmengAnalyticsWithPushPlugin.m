a8ad64943c36c07e0cc3444f624109a103259c11
coscx <710666260@qq.com>
2022-04-22 15:21:22 +0800

    add

--- ios/Classes/UmengAnalyticsWithPushPlugin.m
+++ ios/Classes/UmengAnalyticsWithPushPlugin.m
@@ -33,21 +33,23 @@ FlutterEventSink _eventSink;
       [self initUmeng:call result:result];
   } else if ([@"initialize" isEqualToString:call.method]) {
       [self initialize:call result:result];
-  }else if ([@"addTags" isEqualToString:call.method]) {
+  }else if ([@"addPushTags" isEqualToString:call.method]) {
       [self addTags:call result:result];
-  } else if ([@"deleteTags" isEqualToString:call.method]) {
+  }else if ([@"getPushTags" isEqualToString:call.method]) {
+      [self getTags:call result:result];
+  }  else if ([@"removePushTags" isEqualToString:call.method]) {
       [self deleteTags:call result:result];
-  } else if ([@"addAlias" isEqualToString:call.method]) {
+  } else if ([@"addPushAlias" isEqualToString:call.method]) {
       [self addAlias:call result:result];
-  } else if ([@"setAlias" isEqualToString:call.method]) {
+  } else if ([@"putPushAlias" isEqualToString:call.method]) {
       [self setAlias:call result:result];
-  } else if ([@"deleteAlias" isEqualToString:call.method]) {
+  } else if ([@"removePushAlias" isEqualToString:call.method]) {
       [self deleteAlias:call result:result];
-  } else if ([@"pageStart" isEqualToString:call.method]) {
+  } else if ([@"onPageStart" isEqualToString:call.method]) {
       [self pageStart:call result:result];
-  } else if ([@"pageEnd" isEqualToString:call.method]) {
+  } else if ([@"onPageEnd" isEqualToString:call.method]) {
       [self pageEnd:call result:result];
-  } else if ([@"event" isEqualToString:call.method]) {
+  } else if ([@"onEvent" isEqualToString:call.method]) {
       [self event:call result:result];
   } else if ([@"deviceToken" isEqualToString:call.method]) {
     [self deviceToken:call result:result];
@@ -57,8 +59,9 @@ FlutterEventSink _eventSink;
 }
 
 - (void)event:(FlutterMethodCall*)call result:(FlutterResult)result {
-    NSString* eventId = call.arguments[@"eventId"];
-    NSString* label = call.arguments[@"label"];
+
+    NSString* eventId = call.arguments[@"event"];
+    NSString* label = call.arguments[@"params"];
     if (label == nil) {
         [MobClick event:eventId];
     } else {
@@ -73,12 +76,12 @@ FlutterEventSink _eventSink;
 }
 
 - (void)pageStart:(FlutterMethodCall*)call result:(FlutterResult)result {
-  NSString* pageName = call.arguments[@"pageName"];
+  NSString* pageName = [call.arguments NSString*];
   [MobClick beginLogPageView:pageName];
 }
 
 - (void)pageEnd:(FlutterMethodCall*)call result:(FlutterResult)result {
-  NSString* pageName = call.arguments[@"pageName"];
+  NSString* pageName = [call.arguments  NSString*];
   [MobClick endLogPageView:pageName];
 }
 - (void)initialize:(FlutterMethodCall *)call result:(FlutterResult)result {
@@ -88,34 +91,37 @@ FlutterEventSink _eventSink;
   result(@"initialize");
 }
 - (void)addTags:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *tags = call.arguments[@"tags"];
+  NSString *tags = [call.arguments NSString*];
   [UMessage addTags:tags response:^(id  _Nonnull responseObject, NSInteger remain, NSError * _Nonnull error) {
   }];
 }
-
+- (void)getTags:(FlutterMethodCall *)call result:(FlutterResult)result {
+  [UMessage getTags response:^(id  _Nonnull responseObject, NSInteger remain, NSError * _Nonnull error) {
+  }];
+}
 - (void)deleteTags:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *tags = call.arguments[@"tags"];
+  NSString *tags = [call.arguments NSString*];
   [UMessage deleteTags:tags response:^(id  _Nonnull responseObject, NSInteger remain, NSError * _Nonnull error) {
   }];
 }
 
 - (void)addAlias:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *alias = call.arguments[@"alias"];
-  NSString *type = call.arguments[@"type"];
+  NSString *alias = call.arguments[@"alias-value"];
+  NSString *type = call.arguments[@"alias-type"];
   [UMessage addAlias:alias type:type response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
   }];
 }
 
 - (void)setAlias:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *alias = call.arguments[@"alias"];
-  NSString *type = call.arguments[@"type"];
+  NSString *alias = call.arguments[@"alias-value"];
+  NSString *type = call.arguments[@"alias-type"];
   [UMessage setAlias:alias type:type response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
   }];
 }
 
 - (void)deleteAlias:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *alias = call.arguments[@"alias"];
-  NSString *type = call.arguments[@"type"];
+  NSString *alias = call.arguments[@"alias-value"];
+  NSString *type = call.arguments[@"alias-type"];
   [UMessage removeAlias:alias type:type response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
   }];
 }
